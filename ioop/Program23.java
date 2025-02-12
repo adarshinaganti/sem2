ab7117100098033cadc18af049904144fd004265
adarsh <adarshinaganti006@gmail.com>
2025-02-12 19:45:35 +0530

    added week6

---
+++ ioop/Program23.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Program23 {
+    public static boolean isPalindrome(String str) {
+        String text = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String rev = new StringBuilder(text).reverse().toString();
+        return text.equals(rev);
+    }
+
+    public static String alphabetizeString(String text) {
+        char[] chars = text.toCharArray();
+        java.util.Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    public static String reverseString(String text) {
+        return new StringBuilder(text).reverse().toString();
+    }
+
+    public static String concatenate(String str1, String str2) {
+        return str1 + str2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.next();
+
+        while (true) {
+            System.out.println("Menu:");
+            System.out.println("1. Check if palindrome");
+            System.out.println("2. Alphabetize string");
+            System.out.println("3. Reverse string");
+            System.out.println("4. Concatenate original and reversed string");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter your choice (1-5): ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    if (isPalindrome(str)) {
+                        System.out.println("The string is a palindrome.");
+                    } else {
+                        System.out.println("The string is not a palindrome.");
+                    }
+                    break;
+                case 2:
+                    String alphabetized = alphabetizeString(str);
+                    System.out.println("Alphabetized string: " + alphabetized);
+                    break;
+                case 3:
+                    String reversedString = reverseString(str);
+                    System.out.println("Reversed string: " + reversedString);
+                    break;
+                case 4:
+                    String reversedStringConcat = reverseString(str);
+                    String concatenatedString = concatenate(str, reversedStringConcat);
+                    System.out.println("Concatenated string: " + concatenatedString);
+                    break;
+                case 5:
+                    System.out.println("Exiting program.");
+                    sc.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 5.");
+            }
+        }
+    }
+}
---
+++ ioop/Program25.java
@@ -0,0 +1,99 @@
+// Student class
+
+import java.util.Scanner;
+
+class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String extractInitials() {
+        String[] parts = name.split(" ");
+        String initials = "";
+        for (String part : parts) {
+            if (!part.isEmpty()) {
+                initials += part.charAt(0);
+            }
+        }
+        return initials;
+    }
+
+    public String removeWhitespace() {
+        return name.replaceAll("\\s+", "");
+    }
+
+    public boolean containsSubstring(String substring) {
+        return name.contains(substring);
+    }
+
+    public static int compareNames(Student s1, Student s2) {
+        return s1.getName().compareToIgnoreCase(s2.getName());
+    }
+    
+    public String toString() {
+        return name;
+    }
+}
+
+public class Program25 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Student[] students = new Student[100];
+        int count = 0;
+
+        System.out.println("Enter student names (type 'done' to finish):");
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("done")) {
+                break;
+            }
+            students[count++] = new Student(input);
+        }
+
+        System.out.println("\nStudent Details:");
+        for (int i = 0; i < count; i++) {
+            System.out.println(students[i]);
+        }
+
+        System.out.println("\nInitials of Students:");
+        for (int i = 0; i < count; i++) {
+            System.out.println(students[i].getName() + ": " + students[i].extractInitials());
+        }
+
+        System.out.println("\nNames without Whitespace:");
+        for (int i = 0; i < count; i++) {
+            System.out.println(students[i].getName() + ": " + students[i].removeWhitespace());
+        }
+        
+        System.out.print("\nEnter a substring to search: ");
+        String substring = sc.nextLine();
+        System.out.println("\nStudents containing substring \"" + substring + "\":");
+        for (int i = 0; i < count; i++) {
+            if (students[i].containsSubstring(substring)) {
+                System.out.println(students[i]);
+            }
+        }
+
+        for (int i = 0; i < count - 1; i++) {
+            for (int j = i + 1; j < count; j++) {
+                if (Student.compareNames(students[i], students[j]) > 0) {
+                    Student temp = students[i];
+                    students[i] = students[j];
+                    students[j] = temp;
+                }
+            }
+        }
+        System.out.println("\nStudents sorted alphabetically:");
+        for (int i = 0; i < count; i++) {
+            System.out.println(students[i]);
+        }
+
+        sc.close();
+	}
+}
