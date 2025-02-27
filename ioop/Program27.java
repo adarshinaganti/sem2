4bf802fa57af05461f97e0c05151934213de7564
adarsh <adarshinaganti006@gmail.com>
2025-02-27 20:16:45 +0530

    added week7

---
+++ ioop/Program27.java
@@ -0,0 +1,125 @@
+// Employee class
+
+class Employee {
+	private int eID;
+	private String eName;
+	private double baseSal;
+	
+	public Employee() {
+		this.eID = 0;
+		this.eName = "";
+		this.baseSal = 0;
+	}
+	
+	public Employee(int eID, String eName, double baseSal) {
+		this.eID = eID;
+		this.eName = eName;
+		this.baseSal = baseSal;
+	}
+	
+	public int get_eID() {
+		return eID;
+	}
+	
+	public String get_eName() {
+		return eName;
+	}
+	
+	public double get_baseSal() {
+		return baseSal;
+	}
+	
+	public double calcSal() {
+		return baseSal;
+	}
+	
+	public void display() {
+		System.out.println("Employee Details:");
+        System.out.println("Employee ID: " + eID);
+        System.out.println("Name: " + eName);
+        System.out.println("Base Salary: " + baseSal);
+        System.out.println("Calculated Salary: " + calcSal());
+	}
+}
+
+class PartTimeEmp extends Employee {
+	private int hoursWorked;
+	private static final double hourlyRate = 25.0;
+	
+	public PartTimeEmp(int eID, String eName, double baseSal, int hoursWorked) {
+        super(eID, eName, baseSal);
+        this.hoursWorked = hoursWorked;
+    }
+	
+	public int get_HoursWorked() {
+        return hoursWorked;
+    }
+	
+	public static double get_HourlyRate() {
+        return hourlyRate;
+    }
+	
+	@Override
+    public double calcSal() {
+        return hoursWorked * hourlyRate;
+    }
+	
+	@Override
+	public void display() {
+		super.display();
+        System.out.println("Hours Worked: " + hoursWorked);
+        System.out.println("Hourly Rate: " + hourlyRate);
+        System.out.println("Calculated Salary (Part-Time): " + calcSal());
+	}
+}
+
+class FullTimeEmp extends Employee {
+	private double bonus;
+	private double deductions;
+	
+	public FullTimeEmp(int eID, String eName, double baseSal, double bonus, double deductions) {
+		super(eID, eName, baseSal);
+		this.bonus = bonus;
+		this.deductions = deductions;
+	}
+	
+	public double getBonus() {
+		return bonus;
+	}
+	
+	public double getDeductions() {
+		return deductions;
+	}
+	
+	@Override
+	public double calcSal() {
+		return get_baseSal() + bonus - deductions;
+	}
+	
+	@Override
+	public void display() {
+		super.display();
+		System.out.println("Bonus: " + bonus);
+        System.out.println("Deductions: " + deductions);
+        System.out.println("Calculated salary: " + calcSal());
+	}
+}
+
+public class Program27 {
+	public static void main(String[] args) {
+		Employee genericEmployee = new Employee(1, "abc", 5000);
+		PartTimeEmp partTimeEmployee = new PartTimeEmp(2, "def", 5000, 10);
+		FullTimeEmp fullTimeEmployee = new FullTimeEmp(3, "ghi", 5000, 1000, 100);
+		
+		// Polymorphism
+		Employee[] e = new Employee[3];
+		e[0] = genericEmployee;
+		e[1] = partTimeEmployee;
+		e[2] = fullTimeEmployee;
+		
+		for (Employee emp : e) {
+			emp.display();
+			System.out.println();
+		}
+	}
+}
---
+++ ioop/Program28.java
@@ -0,0 +1,141 @@
+// Building class
+
+class Building {
+	private double sqft;
+	private int stories;
+	
+	public Building() {
+		this.sqft = 0;
+		this.stories = 0;
+	}
+	
+	public Building(double sqft, int stories) {
+		this.sqft = sqft;
+		this.stories = stories;
+	}
+	
+	public double get_sqft() {
+		return sqft;
+	}
+	
+	public int get_stories() {
+		return stories;
+	}
+	
+	public void set_sqft(double sqft) {
+		this.sqft = sqft;
+	}
+	
+	public void set_stories(int stories) {
+		this.stories = stories;
+	}
+	
+	public void display() {
+		System.out.println("Building Details:");
+        System.out.println("Square Footage: " + sqft);
+        System.out.println("Number of Stories: " + stories);
+	}
+}
+
+class House extends Building {
+	private int bedrooms;
+	private int baths;
+	
+	public House() {
+		super();
+		this.bedrooms = 0;
+		this.baths = 0;
+	}
+	
+	public House(double sqft, int stories, int bedrooms, int baths) {
+		super(sqft, stories);
+		this.bedrooms = bedrooms;
+		this.baths = baths;
+	}
+	
+	public int get_bedrooms() {
+        return bedrooms;
+    }
+
+    public int get_baths() {
+        return baths;
+    }
+    
+    public void set_bedrooms(int bedrooms) {
+        this.bedrooms = bedrooms;
+    }
+
+    public void set_baths(int baths) {
+        this.baths = baths;
+    }
+    
+    @Override
+    public void display() {
+    	super.display();
+    	System.out.println();
+    	System.out.println("House details: ");
+    	System.out.println("Number of bedrooms: " + bedrooms);
+    	System.out.println("Number of baths: " + baths);
+    }
+}
+
+class School extends Building {
+	private int classrooms;
+	private String gradeLevel;
+	
+	public School() {
+		super();
+		this.classrooms = 0;
+		this.gradeLevel = "";
+	}
+	
+	public School(double sqft, int stories, int classrooms, String gradeLevel) {
+		super(sqft, stories);
+		this.classrooms = classrooms;
+		this.gradeLevel = gradeLevel;
+	}
+	
+	public int get_classrooms() {
+        return classrooms;
+    }
+
+    public String get_gradeLevel() {
+        return gradeLevel;
+    }
+    
+    public void set_classrooms(int classrooms) {
+        this.classrooms = classrooms;
+    }
+
+    public void set_gradeLevel(String gradeLevel) {
+        this.gradeLevel = gradeLevel;
+    }
+    
+    @Override
+    public void display() {
+    	super.display();
+    	System.out.println();
+    	System.out.println("School details: ");
+    	System.out.println("Number of classrooms: " + classrooms);
+    	System.out.println("Grade level: " + gradeLevel);
+    }
+}
+
+public class Program28 {
+	public static void main(String[] args) {
+		Building genericBuilding = new Building(5000, 3);
+		House house = new House(8000, 2, 3, 2);
+		School school = new School(10000, 4, 10, "High");
+		
+		// Polymorphism
+		Building[] buildings = new Building[3];
+		buildings[0] = genericBuilding;
+		buildings[1] = house;
+		buildings[2] = school;
+		
+		for (Building building : buildings) {
+			building.display();
+			System.out.println();
+		}
+	}
+}
