505404b538260c5c687ba41df4900349eaf41437
Adarsh Inaganti <adarshinaganti006@gmail.com>
2025-03-13 11:45:37 +0530

    added week8

---
+++ ioop/Program31.java
@@ -0,0 +1,53 @@
+// Series interface
+
+interface Series {
+	int getNext();
+	void reset();
+	void setStart(int start);
+}
+
+class ByTwos implements Series {
+	int startValue, currentValue;
+	
+	ByTwos() {
+		startValue = 0;
+		currentValue = startValue;
+	}
+	
+	@Override
+	public int getNext() {
+		currentValue += 2;
+		return currentValue;
+	}
+	
+	@Override
+	public void reset() {
+		currentValue = startValue;
+	}
+	
+	@Override
+	public void setStart(int start) {
+		startValue = start;
+		currentValue = startValue;
+	}
+}
+
+public class Program31 {
+	public static void main(String[] args) {
+		Series series_ByTwos = new ByTwos();
+		
+		System.out.println("Starting from default value (zero):");
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Next: " + series_ByTwos.getNext());
+		}
+		
+		System.out.println("\nResetting:");
+		series_ByTwos.reset();
+		System.out.println("Next after reset: " + series_ByTwos.getNext());
+		
+		System.out.println("\nSetting start value to 10:");
+		series_ByTwos.setStart(10);
+		System.out.println("Next after setting start to 10: " + series_ByTwos.getNext());
+		System.out.println("Next: " + series_ByTwos.getNext());
+	}
+}
